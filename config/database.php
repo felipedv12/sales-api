4c82ed1cee2ed0935dc96b7c7aa56c34a1df19cf
Felipe Marques <felipedv12@hotmail.com>
2023-04-18 15:35:59 -0300

    update: ajustando arquivo de configuração do bd

--- config/database.php
+++ config/database.php
@@ -1,3 +1,16 @@
 <?php
 
-CONST PG_PASSWORD = 'Pg17042023*';
+namespace Config;
+
+const PG_PASSWORD = 'Pg17042023*';
+
+class Database
+{
+    const DB_DRIVER = 'pgsql';
+    const DB_HOST = 'localhost';
+    const DB_PORT = '5432';
+    const DB_USER = 'salesapi';
+    const DB_PASSWORD = 'Sl17042023*';
+    const DB_NAME = 'sales_db';
+
+}
